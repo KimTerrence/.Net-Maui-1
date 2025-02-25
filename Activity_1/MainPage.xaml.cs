e36f857fe67033f51df1b66fda3dc28cd662d687
KimTerrence <quineskimterrence@gmail.com>
2025-02-25 14:38:09 +0800

    Add project files.

---
+++ Activity_1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace Activity_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void LoginClk(object sender, EventArgs e)
+        {
+            DisplayAlert("Warning", "Username not found", "OK");
+        }
+        private void GoTaker(object sender, EventArgs e)
+        {
+            TakerScreen.IsVisible = true;
+            WelcomeSreen.IsVisible = false;
+        }
+        private void GoLogin(object sender, EventArgs e)
+        {
+            LoginScreen.IsVisible = true;
+            WelcomeSreen.IsVisible = false;
+        }
+        private void Exit(object sender, EventArgs e)
+        {
+            WelcomeSreen.IsVisible = true;
+            TakerScreen.IsVisible = false;
+            LoginScreen.IsVisible = false;
+        }
+    }
+
+}
---
+++ Activity_1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Activity_1;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
